8e04c125302721b349d9471cd9c925d26d18bcaf
Kaarthik <skaarthik21@gmail.com>
2022-01-14 02:14:47 +0530

    new-page

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+    // Show hide popover
+    $(".MENU").click(function(){
+        $(this).find(".sidenav").slideToggle("fast");
+    });
+});
+$(document).on("click", function(event){
+    var $trigger = $(".MENU");
+    if($trigger !== event.target && !$trigger.has(event.target).length){
+        $(".sidenav").slideUp("fast");
+    }            
+});
+
+
+
